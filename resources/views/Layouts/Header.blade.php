aeef3a92bf66af268013603332dc0bcd4362e27a
MuhaddiMu <muhaddisshah@gmail.com>
2019-04-19 22:00:20 +0500

    Favicon Updated

--- resources/views/Layouts/Header.blade.php
+++ resources/views/Layouts/Header.blade.php
@@ -7,7 +7,7 @@
 
     <!-- Material Design for Bootstrap fonts and icons -->
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Material+Icons">
-
+		<link rel="icon" type="image/png" href="{{asset('favicon.ico')}}">
     <!-- Material Design for Bootstrap CSS -->
     <link rel="stylesheet" href="https://unpkg.com/bootstrap-material-design@4.1.1/dist/css/bootstrap-material-design.min.css" integrity="sha384-wXznGJNEXNG1NFsbm0ugrLFMQPWswR3lds2VeinahP8N0zJw9VWSopbjv2x7WCvX" crossorigin="anonymous">
 

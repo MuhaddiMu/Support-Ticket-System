2eb994043b431afedccb9f5bcfd10934c0c57d10
MuhaddiMu <muhaddisshah@gmail.com>
2019-04-20 12:42:58 +0500

    Old Helper Function Created

--- resources/views/tickets/create.blade.php
+++ resources/views/tickets/create.blade.php
@@ -13,11 +13,11 @@
 
 						<div class="form-group">
 						  <label for="Title" class="bmd-label-floating">Title</label>
-						  <input type="text" name="Title" id="Title" class="form-control">
+						  <input type="text" name="Title" id="Title" value="{{ old('Title') }}" class="form-control">
 						</div>
 						<div class="form-group">
 						  <label for="Content" class="bmd-label-floating">Content</label>
-						  <textarea id="Content" name="Content" class="form-control" rows="3"></textarea>
+						  <textarea id="Content" name="Content" class="form-control" rows="3">{{ old('Content') }}</textarea>
 						  <span class="bmd-help">Feel free to ask us any question</span>
 						</div>  
 						<a class="btn btn-default" href="{{ URL::previous() }}">Cancel</a>
